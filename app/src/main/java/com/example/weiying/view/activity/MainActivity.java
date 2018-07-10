94f6cde284eb9f11ab63a02187c234e11cbb77ed
fzy123456789 <1052877505@qq.com>
2018-07-10 10:19:05 +0800

    搞定欢迎界面,实现完美底部导航栏!!!

--- app/src/main/java/com/example/weiying/view/activity/MainActivity.java
+++ app/src/main/java/com/example/weiying/view/activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.weiying.view.activity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.weiying.R;
 import com.example.weiying.presenter.MainPresenter;
@@ -17,7 +18,7 @@ import javax.inject.Inject;
 public class MainActivity extends BaseActivity<MainPresenter> {
     
     @Inject
-    public MainPresenter mainPresenter;
+    MainPresenter mainPresenter;
     private BottomTabBar mMainTabBar;
 
     @Override
@@ -35,21 +36,21 @@ public class MainActivity extends BaseActivity<MainPresenter> {
     @Override
     void initData() {
         mMainTabBar.init(getSupportFragmentManager())
-                .setImgSize(43,47)
+                .setImgSize(60,60)
                 .setFontSize(20)
-                .setTabPadding(4,6,10)
+                .setTabPadding(20,12,0)
                 .setChangeColor(Color.RED,Color.GRAY)
                 .addTabItem("精选",R.drawable.found_select,R.drawable.found, SiftFragment.class)
                 .addTabItem("专题",R.drawable.special_select,R.drawable.special, FindFragment.class)
                 .addTabItem("发现",R.drawable.fancy_select,R.drawable.fancy, SiftFragment.class)
-                .addTabItem("现场",R.drawable.special_select,R.drawable.special, SceneFragment.class)
+                .addTabItem("现场",R.drawable.shipin,R.drawable.video, SceneFragment.class)
                 .addTabItem("我的",R.drawable.my_select,R.drawable.my, MyFragment.class)
                 .setTabBarBackgroundResource(R.drawable.bottom_bg)
                 .setOnTabChangeListener(new BottomTabBar.OnTabChangeListener() {
-            @Override
-            public void onTabChange(int position, String name) {
-            }
-        });
+                    @Override
+                    public void onTabChange(int position, String name, View view) {
+                    }
+                });
     }
     @Override
     MainPresenter setBasePresenter() {
--- app/src/main/java/com/example/weiying/view/activity/WelcomeActivity.java
+++ app/src/main/java/com/example/weiying/view/activity/WelcomeActivity.java
@@ -1,34 +1,74 @@
 package com.example.weiying.view.activity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.widget.ImageView;
 
 import com.example.weiying.R;
 import com.example.weiying.presenter.BasePresenter;
 
-public class WelcomeActivity extends BaseActivity{
+import java.util.Random;
+
+public class WelcomeActivity extends AppCompatActivity {
+    ImageView iv_entry;
+
+    private static final int ANIM_TIME = 2000;
+
+    private static final float SCALE_END = 1.15F;
+
+    private static final int[] Imgs={R.drawable.a,R.drawable.b, R.drawable.c,R.drawable.d, R.drawable.e, R.drawable.f, R.drawable.g};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        iv_entry = findViewById(R.id.iv_entry);
+        Random random = new Random(SystemClock.elapsedRealtime());//SystemClock.elapsedRealtime() 从开机到现在的毫秒数（手机睡眠(sleep)的时间也包括在内）
+        iv_entry.setImageResource(Imgs[random.nextInt(Imgs.length)]);
+
+        setValueAnimator();
     }
 
-    @Override
-    void initView() {
-        
+    private void setValueAnimator() {
+        ValueAnimator valueAnimator = ValueAnimator.ofFloat(1f, SCALE_END);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float value = (float) animation.getAnimatedValue();
+                iv_entry.setScaleX(value);
+                iv_entry.setScaleY(value);
+            }
+        });
+        valueAnimator.setDuration(ANIM_TIME);
+        valueAnimator.start();
+        valueAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+                overridePendingTransition(0,0);
+                WelcomeActivity.this.finish();
+                super.onAnimationEnd(animation);
+            }
+        });
     }
 
+    /**
+     * 屏蔽物理返回按钮
+     * @param keyCode
+     * @param event
+     * @return
+     */
     @Override
-    void initData() {
-
-    }
-
-    @Override
-    BasePresenter setBasePresenter() {
-        return null;
-    }
-
-    @Override
-    int setChildContentView() {
-        return R.layout.activity_welcome;
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode==KeyEvent.KEYCODE_BACK){
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
